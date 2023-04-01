d10f84d0f4eb3234ad416e9d0fc8fae1c7eb0de6
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-04-01 21:41:31 +0530

    Create Binary Search.cpp

---
+++ Binary Search/Binary Search.cpp
@@ -0,0 +1,33 @@
+// Link: 
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int n = nums.size() ;
+
+        int low = 0 ;
+
+        int high = n - 1 ;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2 ;
+
+            if(nums[mid] == target)
+                return mid ;
+
+            else if(nums[mid] < target)
+                low = mid + 1 ;
+
+            else
+                high = mid - 1 ;
+
+        }   
+
+        return -1 ;
+    }
+};
+
+// Time Complexity:  O(Log N)
+// Space Complexity: O(1)
