caa61475a838893de9bc6362c7d2732dfb799201
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-04-27 10:16:01 +0530

    Create Bulb Switcher.cpp

---
+++ Maths/Bulb Switcher.cpp
@@ -0,0 +1,12 @@
+// Link: https://leetcode.com/problems/bulb-switcher/description/
+
+class Solution {
+public:
+    int bulbSwitch(int n) 
+    {
+        return sqrt(n) ;    
+    }
+};
+
+// Time Complexity:  O(1) 
+// Space Complexity: O(1)
